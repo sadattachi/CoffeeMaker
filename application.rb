8d55ce25412462d39dffe49e61f42cdf9f45e87d
sadattachi <nazarii200322@gmail.com>
2022-05-31 13:49:19 +0300

    Add different coffee machines

--- application.rb
+++ application.rb
@@ -1,53 +1,75 @@
-require './coffeemaker'
- 
+require "./coffeemaker"
+
 module Application
-   def self.run
-       menu = Menu.new
-       begin
-           menu.printMenu
-           choice = gets.chomp
-           case choice
-               when 'a' then menu.switchPower
-               when 'b' then menu.fillTank
-               when 'c' then menu.addGround
-               when 'd' then menu.makeCoffee
-           end
-       end while choice != 'q'
-   end
-   class Menu
-       def initialize
-           @coffeemaker = FastCoffeeMaker.new
-       end
-       def printMenu
-           system("clear")
-           puts "Coffee Maker".center(50)
-           puts "State: #{@coffeemaker.powerState ? 'on' : 'off'}"
-           puts "Coffee grounds: #{@coffeemaker.coffeeInserted ? 'inserted' : 'missing'}"
-           puts "Water tank: #{@coffeemaker.currentWaterCapacity}ml"
-           puts "a - Power Switch"
-           puts "b - Fill water tank"
-           puts "c - Add coffee ground"
-           puts "d - Make coffee"
-           puts "q - Quit"
-           print "Enter your choice: "
-       end
-       def switchPower
-           @coffeemaker.switchPower
-       end
-       def fillTank
-           @coffeemaker.fillTank
-       end
-       def addGround
-           @coffeemaker.addGround
-       end
-       def makeCoffee
-           begin
-               @coffeemaker.makeCoffee
-           rescue => e
-               puts e.message
-               sleep 2
-           end
-       end
- 
-   end
+  def self.run
+    menu = Menu.new
+    menu.selectCoffeeMaker
+    begin
+      menu.printMenu
+      choice = gets.chomp
+      case choice
+      when "a" then menu.switchPower
+      when "b" then menu.fillTank
+      when "c" then menu.addGround
+      when "d" then menu.makeCoffee
+      end
+    end while choice != "q"
+  end
+
+  class Menu
+    def initialize
+      @coffeemaker = nil
+    end
+
+    def selectCoffeeMaker
+      system("clear")
+      puts "Select Coffee Maker".center(50)
+      puts "a - Normal"
+      puts "b - Fast"
+      puts "c - For Groups"
+      print "Enter your choice: "
+      choice = gets.chomp
+
+      case choice
+      when "a" then @coffeemaker = NormalCoffeeMaker.new
+      when "b" then @coffeemaker = FastCoffeeMaker.new
+      when "c" then @coffeemaker = GroupCoffeeMaker.new
+      end
+    end
+
+    def printMenu
+      system("clear")
+      puts @coffeemaker.getName.center(50)
+      puts "State: #{@coffeemaker.powerState ? "on" : "off"}"
+      puts "Coffee grounds: #{@coffeemaker.coffeeInserted ? "inserted" : "missing"}"
+      puts "Water tank: #{@coffeemaker.currentWaterCapacity}ml"
+      puts "a - Power Switch"
+      puts "b - Fill water tank"
+      puts "c - Add coffee ground"
+      puts "d - Make coffee"
+      puts "q - Quit"
+      print "Enter your choice: "
+    end
+
+    def switchPower
+      @coffeemaker.switchPower
+    end
+
+    def fillTank
+      @coffeemaker.fillTank
+    end
+
+    def addGround
+      @coffeemaker.addGround
+    end
+
+    def makeCoffee
+      begin
+        @coffeemaker.makeCoffee
+      rescue => e
+        puts e.message
+        sleep 2
+      end
+    end
+  end
 end
--- coffeemaker.rb
+++ coffeemaker.rb
@@ -1,46 +1,83 @@
-require './exceptions'
- 
+require "./exceptions"
+
 class CoffeeMaker
-   attr_reader :powerState, :currentWaterCapacity, :coffeeInserted
-   def initialize(maxCapacity = 1500)
-       @powerState = false
-       @coffeeInserted = false
-       @maxWaterCapacity = maxCapacity
-       @currentWaterCapacity = 0
-   end
-   def switchPower
-       @powerState = !@powerState
-   end
-   def fillTank
-       @currentWaterCapacity = @maxWaterCapacity
-   end
-   def addGround
-       @coffeeInserted = true
-   end
-   def makeCoffee(speed = 0.5, message = "Enjoy your coffee!")
-       raise NoPowerException.new("No power!") unless @powerState
-       raise NotEnoughWaterException.new("Not enough water!") if @currentWaterCapacity < 250
-       raise NoCoffeeException.new("No coffee!") unless @coffeeInserted
-       system("clear")
-       10.step(100, 10) {|i|
-           puts "Boiling water #{i} degrees"
-           sleep speed
-           system("clear")
-       }
-       puts message
-       sleep 3
-       @coffeeInserted = false
-       @currentWaterCapacity -= 250
-   end
- 
+  attr_reader :powerState, :currentWaterCapacity, :coffeeInserted
+
+  def initialize(maxCapacity, boilingSpeed)
+    @powerState = false
+    @coffeeInserted = false
+    @maxWaterCapacity = maxCapacity
+    @currentWaterCapacity = 0
+    @boilingSpeed = boilingSpeed
+  end
+
+  def switchPower
+    @powerState = !@powerState
+  end
+
+  def fillTank
+    @currentWaterCapacity = @maxWaterCapacity
+  end
+
+  def addGround
+    @coffeeInserted = true
+  end
+
+  def makeCoffee(message)
+    raise NoPowerException.new("No power!") unless @powerState
+    raise NotEnoughWaterException.new("Not enough water!") if @currentWaterCapacity < 250
+    raise NoCoffeeException.new("No coffee!") unless @coffeeInserted
+    system("clear")
+    10.step(100, 10) { |i|
+      puts "Boiling water #{i} degrees"
+      sleep @boilingSpeed
+      system("clear")
+    }
+    puts message
+    sleep 3
+    @coffeeInserted = false
+    @currentWaterCapacity -= 250
+  end
 end
- 
+
 class FastCoffeeMaker < CoffeeMaker
-   def initialize
-       super(500)
-   end
-   def makeCoffee(speed = 0.1, message = "Enjoy your fast coffee!")
-       super
-   end
- 
+  def initialize
+    super(500, 0.3)
+  end
+
+  def getName
+    "Fast Coffee Maker"
+  end
+
+  def makeCoffee(message = "Enjoy your fast coffee!")
+    super
+  end
+end
+
+class NormalCoffeeMaker < CoffeeMaker
+  def initialize
+    super(1500, 0.5)
+  end
+
+  def getName
+    "Coffee Maker"
+  end
+
+  def makeCoffee(message = "Enjoy your coffee!")
+    super
+  end
+end
+
+class GroupCoffeeMaker < CoffeeMaker
+  def initialize
+    super(3000, 1.5)
+  end
+
+  def getName
+    "Group Coffee Maker"
+  end
+
+  def makeCoffee(message = "Enjoy your coffee with friends!")
+    super
+  end
 end
